6c24de8cf17c1dee233ff35c4186c804d56b150e
Kangboom <jkl6147@naver.com>
2024-10-15 22:18:01 +0900

    init: 초기프로젝트 세팅

---
+++ src/main/java/com/imagine/TechLinker/TechLinkerApplication.java
@@ -0,0 +1,13 @@
+package com.imagine.TechLinker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechLinkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechLinkerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/imagine/TechLinker/TechLinkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imagine.TechLinker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechLinkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
