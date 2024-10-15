d1e29143efc1236344f21970e5869ffd33918a90
임강범 <103165796+kangboom@users.noreply.github.com>
2024-10-15 23:00:43 +0900

    [FEAT] 도메인 엔티티 추가 (#12)
    * init: 초기프로젝트 세팅
    
    * feat: 도메인 엔티티 생성

---
+++ src/main/java/com/imagine/TechLinker/wishlist/Wishlist.java
@@ -0,0 +1,11 @@
+package com.imagine.TechLinker.wishlist;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Wishlist {
+
+    @Id
+    private Long id;
+}
